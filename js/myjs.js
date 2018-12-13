2d11c1144a03851f2f601f154946eb6bb7bfea9c
benkissi <benkissi111@gmail.com>
2018-12-13 20:53:32 +0000

    changed website copy

--- js/myjs.js
+++ js/myjs.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
     $(".newsletter-form").submit(function(e) {
         e.preventDefault();
-
+        let status = $('.info');
         var fname = $('.newsletter-fname').val();
         var email = $('.newsletter-email').val();
 
@@ -30,9 +30,38 @@ $(document).ready(function() {
             status.text(" Please input a valid email :(");
             status.css("color", "red");
           }
-    })
+    });
 
-    var status = $('.info');
+    $("#gform").submit(function(e) {
+        e.preventDefault();
+        let status = $(".form-status");
+        var honeypot = $('#gform input[name=honeypot]').val();
+        var email = $('#gform input[name=email]').val();
+        var formData = $('#gform').serialize();
+
+        if (validateEmail(email)) {
+            if (honeypot == "") {
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://script.google.com/macros/s/AKfycbxEYKh0IGILUXfzmwbfmM28Dja935LpR_hgbYLc/exec',
+                    data: formData,
+                    dataType: 'json'
+                }).done(function(data) {
+                    console.log(data);
+                    status.text("Message has been sent. I will reply you shortly");
+                    status.css("color", "green");
+                })
+            }else {
+            status.text(" Please input a name :(");
+            status.css("color", "red");
+            }
+          } else {
+            status.text(" Please input a valid email :(");
+            status.css("color", "red");
+          }
+    });
+
+    
 
     
 
